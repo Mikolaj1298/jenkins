9a72c6c0c4871f058bb7d8265e8cac5f06422c70
Mikołaj Kamiński <241388@student.pwr.edu.pl>
2020-06-23 00:46:16 +0200

    dodane

---
+++ tests.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import unittest
+import xmlrunner
+
+class TestScrabble(unittest.TestCase):
+
+    def test_open_page(self):
+        try:
+            driver.get("https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna")
+            self.assertTrue(True)
+        except:
+            self.assertFalse(False)
+
+        
+if __name__ == '__main__':
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    unittest.main()
+        
