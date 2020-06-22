f7bb935c5d33f2e2e635c45a18eedb637d117908
Mikolaj_Kaminski <44196908+Mikolaj1298@users.noreply.github.com>
2020-06-23 01:53:57 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -3,18 +3,32 @@ from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 import xmlrunner
 
-class TestScrabble(unittest.TestCase):
+class Test(unittest.TestCase):
 
     def test_case(self):
         try:
-            driver.get("https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna")
+            driver.get("https://www.iban.com/exchange-rates")
             self.assertTrue(True)
         except:
             self.assertFalse(False)
+
+    def test_case2(self):
+        try:
+            driver.get("https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna")
+            trs = driver.find_elements_by_xpath("//tr")
+            counter = 0
+            for tr in trs[1:]:
+                try:
+                    exists = (self.decode(tr.get_attribute('innerHTML')))
+                    if(exists): counter += 1
+                except TypeError:
+                    continue 
+            self.assertTrue(counter)
+        except:
+            self.assertFalse(0)
 
         
 if __name__ == '__main__':
     runner = xmlrunner.XMLTestRunner(output='test-reports')
     unittest.main(testRunner=runner)
     unittest.main()
-        
